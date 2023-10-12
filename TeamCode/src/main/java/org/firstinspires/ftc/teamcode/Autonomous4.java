1d72086203baeced50a1180e57bb71e15e279d17
Adheesh <adheesh.chincholi@gmail.com>
2023-10-11 19:37:20 -0500

    Refined values in MecanumEncoderAutonomous.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous4.java
@@ -0,0 +1,237 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+//All values of the variables target and encoderCounts are reversed due to the encoders
+@Autonomous(name="Autonomous4", group = "Auto")
+public class Autonomous4 extends LinearOpMode {
+    private DcMotor left_front;
+    private DcMotor left_back;
+    private DcMotor right_front;
+    private DcMotor right_back;
+
+    private static final double COUNTS_PER_MOTOR_REV = 756; //Number of encoder counts per motor revolution (1440)
+    private static final double WHEEL_DIAMETER_INCHES = 5.5;
+    private  static final double GEAR_RATIO = 1.0;
+    private static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * GEAR_RATIO)/(WHEEL_DIAMETER_INCHES * Math.PI);
+
+    private static final double ROBOT_WIDTH_INCHES = 25.5; // The distance between the wheels on opposite sides
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() {
+
+        Initialization();
+
+        waitForStart();
+
+        forward(48, 0.5);
+
+    }
+
+
+    public void Initialization(){
+        left_front = hardwareMap.get(DcMotor.class, "left_front");
+        left_back = hardwareMap.get(DcMotor.class, "left_back");
+        right_front = hardwareMap.get(DcMotor.class, "right_front");
+        right_back = hardwareMap.get(DcMotor.class, "right_back");
+
+        right_back.setDirection(DcMotor.Direction.REVERSE);
+        right_front.setDirection(DcMotor.Direction.REVERSE);
+
+        left_front.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        left_back.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        right_front.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        right_back.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        left_front.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        left_back.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        right_front.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        right_back.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public void slowDownAtEnd(double p) {
+        while (opModeIsActive() && left_front.isBusy()) { //Accurate measures
+            double remainingDistance = Math.abs(left_front.getTargetPosition() - left_front.getCurrentPosition());
+            // Adjust this threshold as needed.
+            double rampDownThreshold = 600;  // For example, start ramping down when 300 encoder counts away from target.
+
+            if (remainingDistance < rampDownThreshold) {
+                double rampedSpeed = 0.5 * (p * (remainingDistance / rampDownThreshold));
+                rampedSpeed = Math.max(rampedSpeed, 0.1); // Ensure you have a minimum speed to prevent the robot from stalling.
+                setMotorPower(rampedSpeed);
+            }
+        }
+    }
+
+    public void forward(int distance, double power) {
+        resetEncoders();
+
+        int target = (int) (distance * COUNTS_PER_INCH);
+        left_front.setTargetPosition(-target);
+        left_back.setTargetPosition(-target);
+        right_front.setTargetPosition(-target);
+        right_back.setTargetPosition(-target);
+
+        left_front.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        left_back.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        right_front.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        right_back.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        setMotorPower(power);
+
+
+        slowDownAtEnd(power);
+
+        stopMotor();
+    }
+
+    private void setMotorPower(double x) {
+        left_front.setPower(x);
+        left_back.setPower(x);
+        right_front.setPower(x);
+        right_back.setPower(x);
+    }
+
+    public void backward(int distance, double power) {
+        resetEncoders();
+
+        int target = (int) (distance * COUNTS_PER_INCH);
+        left_front.setTargetPosition(target);
+        left_back.setTargetPosition(target);
+        right_front.setTargetPosition(target);
+        right_back.setTargetPosition(target);
+
+        left_front.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        left_back.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        right_front.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        right_back.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        setMotorPower(power);
+        slowDownAtEnd(power);
+
+        stopMotor();
+
+    }
+
+    public void turn_Left(double degrees, double power) {
+        resetEncoders();
+
+        double radians = Math.toRadians(degrees);
+        double wheelDistance = radians * (ROBOT_WIDTH_INCHES / 2);
+
+        int encoderCounts = (int) (wheelDistance * COUNTS_PER_INCH);
+
+        // Set the target position for a left turn
+        left_front.setTargetPosition(encoderCounts);
+        left_back.setTargetPosition(encoderCounts);
+        right_front.setTargetPosition(-encoderCounts);
+        right_back.setTargetPosition(-encoderCounts);
+
+        left_front.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        left_back.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        right_front.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        right_back.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        // Power the motors to move towards the target position
+        setMotorPower(power);
+
+        slowDownAtEnd(power);
+
+        stopMotor();
+    }
+
+    public void turn_Right(int degrees, double power) {
+        resetEncoders();
+
+        double radians = Math.toRadians(degrees);
+        double wheelDistance = radians * (ROBOT_WIDTH_INCHES / 2);
+
+        int encoderCounts = (int) (wheelDistance * COUNTS_PER_INCH);
+
+        // Set the target position for a left turn
+        left_front.setTargetPosition(-encoderCounts);
+        left_back.setTargetPosition(-encoderCounts);
+        right_front.setTargetPosition(encoderCounts);
+        right_back.setTargetPosition(encoderCounts);
+
+        left_front.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        left_back.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        right_front.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        right_back.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        // Power the motors to move towards the target position
+        setMotorPower(power);
+
+        slowDownAtEnd(power);
+
+        stopMotor();
+    }
+
+    public void strafe_Right(int distance, double power) {
+        resetEncoders();
+
+        int target = (int) (distance * COUNTS_PER_INCH);
+        left_front.setTargetPosition(-target);
+        left_back.setTargetPosition(target);
+        right_front.setTargetPosition(target);
+        right_back.setTargetPosition(-target);
+
+        left_front.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        left_back.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        right_front.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        right_back.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        setMotorPower(power);
+        slowDownAtEnd(power);
+
+        stopMotor();
+
+    }
+
+    public void strafe_Left(int distance, double power) {
+        resetEncoders();
+
+        int target = (int) (distance * COUNTS_PER_INCH);
+        left_front.setTargetPosition(target);
+        left_back.setTargetPosition(-target);
+        right_front.setTargetPosition(-target);
+        right_back.setTargetPosition(target);
+
+        left_front.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        left_back.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        right_front.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        right_back.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        setMotorPower(power);
+        slowDownAtEnd(power);
+
+        stopMotor();
+
+    }
+
+    public void stopMotor() {
+        setMotorPower(0);
+        resetEncoders();
+    }
+
+    private void resetEncoders() {
+        left_front.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        left_back.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        right_front.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        right_back.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        //left_front.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //left_back.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //right_front.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //right_back.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    }
+
+}
+
+
